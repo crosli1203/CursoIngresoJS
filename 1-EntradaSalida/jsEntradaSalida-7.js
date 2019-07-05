444db646d7fd4bf38b6f1a3e19f7be93104e9f9c
crosli1203 <52410334+crosli1203@users.noreply.github.com>
2019-07-05 00:29:40 -0300

    Add files via upload

--- 1-EntradaSalida/jsEntradaSalida-7.js
+++ 1-EntradaSalida/jsEntradaSalida-7.js
@@ -4,21 +4,49 @@ mostrar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
 function sumar()
 {	
-	
+    var numero1;
+    var numero2;
+    
+    numero1=document.getElementById("numeroUno").value;
+  
+    numero2=document.getElementById("numeroDos").value;
+
+      alert("La suma es: "+ (parseInt(numero1)+parseInt(numero2)));
 }
 
 function restar()
 {
-	
+    var numero1;
+    var numero2;
+    
+    numero1=document.getElementById("numeroUno").value;
+  
+    numero2=document.getElementById("numeroDos").value;
+
+      alert("La resta es: "+ (parseInt(numero1)-parseInt(numero2)));
 }
 
 function multiplicar()
 { 
-	
+    var numero1;
+    var numero2;
+    
+    numero1=document.getElementById("numeroUno").value;
+  
+    numero2=document.getElementById("numeroDos").value;
+
+      alert("La multiplicacion es: "+ (parseInt(numero1)*parseInt(numero2)));
 }
 
 function dividir()
 {
 	
-}
+    var numero1;
+    var numero2;
+    
+    numero1=document.getElementById("numeroUno").value;
+  
+    numero2=document.getElementById("numeroDos").value;
 
+      alert("La divicion es: "+ (parseInt(numero1)/parseInt(numero2)));
+    }
